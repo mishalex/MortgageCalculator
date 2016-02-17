cf3337e07dd7d1e3d9ac17a78095d63423f803c0
Alex <mishustin.sv@gmail.com>
2016-02-17 20:18:53 +0300

    index.html and style.css as calculator windows; first connect html and js;

--- code.js
+++ code.js
@@ -1,9 +1,33 @@
-var Sum = 1500000; // todo использовать в именовании lowerCamelCase стиль
-var Months = 240;
-var MonthsPayment; // todo не правильно переведена фраза "ежемесяный платеж"
-var Percent = 13.5;
-MonthsPayment = ((Percent/12/100)*(Math.pow((1+Percent/12/100),Months))/(Math.pow((1+Percent/12/100),Months)-1))*Sum; //todo отформатировать формулу, после запятой всегда и везде ставится пробел, мат. знаки отделяются пробелами от операндов
-console.log(MonthsPayment);
+// var priceJS = document.getElementById("price");
+// sum = priceJS;
+// console.log(sum);
+// // var firstPriceJS = document.getElementById("firstPrice");
+// // sdfsdf = firstPriceJS;
+// // console.log(sum);
+// var yearJS = document.getElementById("year");
+// months = yearJS;
+// console.log(months);
+// var PercentJS = document.getElementById("Percent");
+// percent = PercentJS;
+// console.log(percent);
+
+
+sum = prompt("Сумму кредита");
+months = prompt("Срок выплаты, в месяцах");
+percent = prompt("Процентная ставка");
+
+
+// var sum = 1000; 
+// var months = 120;
+// var percent = 10;
+var monthlyPayment;
+monthlyPayment = ((percent / 12 / 100) * (Math.pow((1 + percent / 12 / 100), months)) / (Math.pow((1 + percent / 12 / 100), months) - 1)) * sum; 
+document.write(monthlyPayment);
+
+
+
+
+
 
 
 
