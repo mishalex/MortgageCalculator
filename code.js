8aa654151354e64ba0cd22d0076fff7a7baff92e
Alex <mishustin.sv@gmail.com>
2016-02-17 22:34:44 +0300

    add code for code.js

--- code.js
+++ code.js
@@ -7,23 +7,43 @@
 // var yearJS = document.getElementById("year");
 // months = yearJS;
 // console.log(months);
-// var PercentJS = document.getElementById("Percent");
-// percent = PercentJS;
-// console.log(percent);
+// // var PercentJS = document.getElementById("Percent");
+// // percent = PercentJS;
+// // console.log(percent);
+// console.log ("dfgdfgd");
 
-
-sum = prompt("Сумму кредита");
-months = prompt("Срок выплаты, в месяцах");
-percent = prompt("Процентная ставка");
+// sum = prompt("Сумму кредита");
+// months = prompt("Срок выплаты, в месяцах");
+// // percent = prompt("Процентная ставка");
 
 
 // var sum = 1000; 
 // var months = 120;
 // var percent = 10;
-var monthlyPayment;
-monthlyPayment = ((percent / 12 / 100) * (Math.pow((1 + percent / 12 / 100), months)) / (Math.pow((1 + percent / 12 / 100), months) - 1)) * sum; 
-document.write(monthlyPayment);
+// // var monthlyPayment;
+// // monthlyPayment = ((percent / 12 / 100) * (Math.pow((1 + percent / 12 / 100), months)) / (Math.pow((1 + percent / 12 / 100), months) - 1)) * sum; 
+// // // // document.write(monthlyPayment);
 
+window.onload = function () {
+	// var sum = document.getElementById ("price").value;
+	// var months = document.getElementById ("year").value;
+	// var percent = document.getElementById ("Percent").value;
+	// var buttom = document.getElementById ("button");
+	
+	// var sum = 1000; 
+	// var months = 120;
+	// var percent = 10;
+	// var monthlyPayment;
+	// monthlyPayment = ((percent / 12 / 100) * (Math.pow((1 + percent / 12 / 100), months)) / (Math.pow((1 + percent / 12 / 100), months) - 1)) * sum; 
+	// console.log(monthlyPayment);
+	// var button = document.getElementById ("button");
+	button.onclick= function() {
+	// alert("click");
+	var result = document.getElementById ("result");
+	result.value = 100000;
+	}
+}
+ 
 
 
 
