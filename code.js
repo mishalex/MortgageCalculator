777c00a68df64c4c6e7f8eb49f2bd4d5b4bc446f
Mishustin Evgeny <e.mishustin@yandex.ru>
2016-02-17 09:46:50 +0300

    add todo to js

--- code.js
+++ code.js
@@ -1,8 +1,8 @@
-var Sum = 1500000;
+var Sum = 1500000; // todo использовать в именовании lowerCamelCase стиль
 var Months = 240;
-var MonthsPayment;
+var MonthsPayment; // todo не правильно переведена фраза "ежемесяный платеж"
 var Percent = 13.5;
-MonthsPayment = ((Percent/12/100)*(Math.pow((1+Percent/12/100),Months))/(Math.pow((1+Percent/12/100),Months)-1))*Sum;
+MonthsPayment = ((Percent/12/100)*(Math.pow((1+Percent/12/100),Months))/(Math.pow((1+Percent/12/100),Months)-1))*Sum; //todo отформатировать формулу, после запятой всегда и везде ставится пробел, мат. знаки отделяются пробелами от операндов
 console.log(MonthsPayment);
 
 
