54f5d5c03f028c07426e5bbefcd7b866faf1590d
mishalex <mishustinav91@yandex.ru>
2016-02-21 23:34:07 +0300

    add calculation

--- code.js
+++ code.js
@@ -1,59 +1,22 @@
-// var priceJS = document.getElementById("price");
-// sum = priceJS;
-// console.log(sum);
-// // var firstPriceJS = document.getElementById("firstPrice");
-// // sdfsdf = firstPriceJS;
-// // console.log(sum);
-// var yearJS = document.getElementById("year");
-// months = yearJS;
-// console.log(months);
-// // var PercentJS = document.getElementById("Percent");
-// // percent = PercentJS;
-// // console.log(percent);
-// console.log ("dfgdfgd");
-
-// sum = prompt("Сумму кредита");
-// months = prompt("Срок выплаты, в месяцах");
-// // percent = prompt("Процентная ставка");
-
-
-// var sum = 1000; 
-// var months = 120;
-// var percent = 10;
-// // var monthlyPayment;
-// // monthlyPayment = ((percent / 12 / 100) * (Math.pow((1 + percent / 12 / 100), months)) / (Math.pow((1 + percent / 12 / 100), months) - 1)) * sum; 
-// // // // document.write(monthlyPayment);
-
-window.onload = function () {
-	// var sum = document.getElementById ("price").value;
-	// var months = document.getElementById ("year").value;
-	// var percent = document.getElementById ("Percent").value;
-	// var buttom = document.getElementById ("button");
-	
-	// var sum = 1000; 
-	// var months = 120;
-	// var percent = 10;
-	// var monthlyPayment;
-	// monthlyPayment = ((percent / 12 / 100) * (Math.pow((1 + percent / 12 / 100), months)) / (Math.pow((1 + percent / 12 / 100), months) - 1)) * sum; 
-	// console.log(monthlyPayment);
-	// var button = document.getElementById ("button");
-	button.onclick= function() {
-	// alert("click");
-	var result = document.getElementById ("result");
-	result.value = 100000;
-	}
+window.onload = function() {	
+	var button = document.getElementById('calculateButton');
+	button.onclick = onCalculateButtonClick;
 }
- 
 
+var onCalculateButtonClick = function() {
+	var price = document.getElementById("price"),
+		months = document.getElementById("months"),
+		percent = document.getElementById("percent"),
+		monthlyPaymentElement = document.getElementById('result');
+	var result = caclulateMonthlyPayment(price.value, months.value, percent.value);
+	document.getElementById('result').value = result;
+}
 
+var caclulateMonthlyPayment = function(price, months, percent) {
+	var monthlyPayment = ((percent / 12 / 100) * (Math.pow((1 + percent / 12 / 100), months)) / (Math.pow((1 + percent / 12 / 100), months) - 1)) * price;
+	return monthlyPayment;
+}
 
-
-
-
-
-
-
-
-
-
-
+var new1 = function(par1) {
+	return 101;
+}
