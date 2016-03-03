6025a86c3a5075500cebe79170103d57fa01cad7
mishalex <mishustinav91@yandex.ru>
2016-03-03 11:41:49 +0300

    validation

--- app.js
+++ app.js
@@ -1,42 +1,46 @@
-window.onload = function() { //todo перенести объявление всех переменных в начало функции, использовать ключевое слово var только один раз, как например в функции-обработчике onCalculateButtonClick
-	var button = document.getElementById('calculateButton');
-	button.onclick = onCalculateButtonClick; 
-	var option = document.getElementById('newselect');
-	option.onchange = onNoneOption;
-	var buttonTable = document.getElementById('buttonTable');
+window.onload = function() { 
+	var button = document.getElementById('calculateButton'),
+		option = document.getElementById('newselect'),
+		buttonTable = document.getElementById('buttonTable');
+	button.onclick = onCalculateButtonClick; 	
+	option.onchange = onNoneOption;	
 	buttonTable.onclick = onButtonTableClick;
 }
 
-var onNoneOption = function() { //todo использовать ключевое слово var только один раз, как например в функции-обработчике onCalculateButtonClick, переименовать opt1-4 во что-нибудь значащее
-	var sum = document.getElementById("opt1");
-	var months = document.getElementById("opt2");
-	var payment = document.getElementById("opt3");
-	var percent = document.getElementById("opt4");
-	var select = document.getElementById("newselect");
-	if (select.selectedIndex == 0) { //todo переделать на switch
+var onNoneOption = function() {
+	var sum = document.getElementById("optSum"),
+		months = document.getElementById("optMonths"),
+		payment = document.getElementById("optPayment"),
+		percent = document.getElementById("optPercent"),
+		select = document.getElementById("newselect");
+	switch (select.selectedIndex) {
+		case 0:
 		sum.style.display = "none";
 		months.style.display = "table-row";
 		payment.style.display = "table-row";
 		percent.style.display = "table-row";
-	}
-	if (select.selectedIndex == 1) {
+		break;
+		
+		case 1:
 		sum.style.display = "table-row";
 		months.style.display = "none";
 		payment.style.display = "table-row";
 		percent.style.display = "table-row";
-	}
-	if (select.selectedIndex == 2) {
+		break;
+		   	   
+   	   case 2:
+   	   	sum.style.display = "table-row";
+		months.style.display = "table-row";
+		payment.style.display = "table-row";
+		percent.style.display = "none";
+		break;
+		
+		case 3:
 		sum.style.display = "table-row";
 		months.style.display = "table-row";
 		payment.style.display = "none";
 		percent.style.display = "table-row";
-	}
-	if (select.selectedIndex == 3) {
-		sum.style.display = "table-row";
-		months.style.display = "table-row";
-		payment.style.display = "table-row";
-		percent.style.display = "none";
-	}
+}
 }
 
 var calculateSum = function(payment, percent, months) {
@@ -49,13 +53,17 @@ var calculateMonthlyPayment = function(price, months, percent) {
 	return  payment;
 }
 
-var calculateMonths = function() { //todo забыл параметры передать
-	var payment = ((percent / 12 / 100) * (Math.pow((1 + percent / 12 / 100), months)) / (Math.pow((1 + percent / 12 / 100), months) - 1)) * price;
-	return months; 
+var calculateMonths = function(price, payment, percent) {
+	var	i = 0;
+	while (price > 0) {
+		i++;
+		price = price - (payment - price * percent / 100 / 12);
+	}
+	return i;
 }
 
 var calculatePercent = function() { //todo реализовать расчет процентов
-	return 'calculate percent';
+	return 'calculatePercent';
 }
 
 var onCalculateButtonClick = function() {
@@ -67,52 +75,53 @@ var onCalculateButtonClick = function() {
 		resultValue,
 		select = document.getElementById("newselect").selectedIndex;
 
-	if (select == 0) { //todo переделать на switch
-		var priceCorrect = validatePriceValues();
-		if (priceCorrect) {
+	switch (select) { 
+		case 0:
+		if (validatePriceValues()) {
 			resultValue = calculateSum(payment.value, percent.value, months.value);
-		} 
-		else {
-			return;
-		}		
-	}
-	if (select == 1) {
-		var monthsCorrect = validateMonthsValues() 
-		if (monthsCorrect) {
-			resultValue = calculateMonths();
 		}
 		else {
 			return;
-		}		
-	}
-	if (select == 2) {
-		var percentCorrect = validatePercentValues();
-		if  (percentCorrect) {
-			resultValue = calculatePercent();
+		}
+		break;
+		
+		case 1: 
+		if (validateMonthsValues()) {
+			resultValue = calculateMonths(price.value, payment.value, percent.value);
 		}
 		else {
 			return;
-		}		
-	}
-	if (select == 3) {
-		var monthlyPaymentCorrect = validateMonthlyPaymentValues();
-		if (monthlyPaymentCorrect) {
+		}
+		break;			
+	
+		case 2:
+		if (validatePercentValues()) {
+		resultValue = calculatePercent();
+		}
+		else {
+			return;
+		}
+		break;				
+	
+		case 3:
+		if (validateMonthlyPaymentValues()) {
 			resultValue = calculateMonthlyPayment(price.value, months.value, percent.value);
 		}
 		else {
 			return;
 		}
-	}
+		break;
+		}
 	result.value = resultValue;
 }
 
-var onButtonTableClick = function() { //todo использовать ключевое слово var только один раз, как например в функции-обработчике onCalculateButtonClick,
- 	var months = document.getElementById("months").value;
- 	var payment = document.getElementById("result").value;
- 	var table = configureTable();
- 	var date = new Date();
- 	var year = date.getFullYear();
- 	var month = date.getMonth();
+var onButtonTableClick = function() {
+ 	var months = document.getElementById("months").value,
+ 		payment = document.getElementById("result").value,
+ 		table = configureTable(),
+ 		date = new Date(),
+ 		year = date.getFullYear(),
+ 		month = date.getMonth();
   	for (var count = 0; count < months; count++) {
 		var firstDate = new Date(year, month + count + 1, 1);
 		createRow(table, count, firstDate, payment);		
@@ -138,26 +147,27 @@ var createRow = function(table, i, date, payment) {
 }
 
 var configureTable = function() {
-	var table = document.createElement('table');//todo использовать ключевое слово var только один раз, как например в функции-обработчике onCalculateButtonClick,
- 	table.id = 't1';
+	var table = document.createElement('table'),
+ 		header = table.createTHead(),
+		row = header.insertRow(0),
+    	cellNumber = row.insertCell(-1),
+    	cellDate = row.insertCell(-1),
+    	cellPayment = row.insertCell(-1);
+    table.id = 't1';
 	table.width = 500;
 	table.border = 1;
 	table.createTHead();
-	var header = table.createTHead();
-	var row = header.insertRow(0);
-    var cellNumber = row.insertCell(-1);
-    var cellDate = row.insertCell(-1);
-    var cellPayment = row.insertCell(-1);
     cellNumber.innerHTML = "<b>№</b>";
     cellDate.innerHTML = "<b>Дата</b>";
     cellPayment.innerHTML = "<b>Платеж</b>";
     return table;
 } 
 
-var validateMonthlyPaymentValues = function() {
+var validateMonthlyPaymentValues = function(form) {
 	var price = document.getElementById("price"),
 		months = document.getElementById("months"),
-		percent = document.getElementById("percent");
+		percent = document.getElementById("percent"),
+		falsePrice = document.getElementById("false");	
 	if (percent.value > 100 || percent.value < 0) {
 		console.log("Введенное значение для процентов указано не верно. Значение процентов должно быть в пределах от 0 до 100");
 		return false;
@@ -167,6 +177,7 @@ var validateMonthlyPaymentValues = function() {
 		return false;
 	}
 	if (price.value > 100000000 || price.value < 10000) {
+		falsePrice.style.display = "table-row";
 		console.log("Введенное значение для цены покупки указано не верно. Цена покупки должна быть в пределах от 100000 до 10000000");
 		return false;
 	}
@@ -195,7 +206,13 @@ var validatePriceValues = function() {
 var validateMonthsValues = function() {
 	var price = document.getElementById("price"),
 		percent = document.getElementById("percent"),
-		payment = document.getElementById("monthlyPayment");
+		payment = document.getElementById("monthlyPayment"),
+		monthlyPayment = document.getElementById("monthlyPayment");
+
+	if (monthlyPayment.value < (price.value * percent.value / 100 / 12)) {
+	console.log("Месячный платеж меньше выплачеваемых процентов по кредиту. Необходима увеличить сумму месячного платежа");
+		return false;
+	}
 	if (percent.value > 100 || percent.value < 0) {
 	console.log("Введенное значение для процентов указано не верно. Значение процентов должно быть в пределах от 0 до 100");
 		return false;
